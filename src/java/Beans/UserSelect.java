53346657e57db3d9ff0275a863f9907a68c82107
Abdullah Salim <sasa_333@live.com>
2020-04-14 10:26:29 +0300

    various changes not so important making the code appear good

--- src/java/Beans/UserSelect.java
+++ src/java/Beans/UserSelect.java
@@ -1,4 +1,3 @@
-
 package Beans;
 
 import javax.faces.bean.ManagedBean;
@@ -11,14 +10,14 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 @RequestScoped
 public class UserSelect {
-    
+
     private String name;
-    private String address ;
-    private String phone ;
+    private String address;
+    private String phone;
     private String email;
     private String username;
-    private String type;   
-   
+    private String type;
+
     public UserSelect() {
     }
 
@@ -69,30 +68,25 @@ public class UserSelect {
     public void setType(String type) {
         this.type = type;
     }
-    
+
     // -------------------------------- functions ----------------------------------------//
     /**
-     * select function selects all the fields of the employee based on the UserName.
-     * and they should be displayed on the text fields.
+     * select function selects all the fields of the employee based on the
+     * UserName. and they should be displayed on the text fields.
      */
     public void select() {
         DB db = new DB();
-        ResultSet rs = null ;
+        ResultSet rs = null;
         try {
-            
+
             String sql = "select * from users where username=?";
             /**
-             * this function establishes connection 
-             * and do the select based on the sql statement operation
-             * and return a result set;
-             *          |
-             *          |
-             *         \ /
-             *          +
+             * this function establishes connection and do the select based on
+             * the sql statement operation and return a result set; | | \ / +
              */
-            
-            rs = db.select(sql, getUsername()); 
-            
+
+            rs = db.select(sql, getUsername());
+
             if (rs.next()) {
                 name = rs.getString("fullname");
                 address = rs.getString("address");
@@ -100,68 +94,63 @@ public class UserSelect {
                 email = rs.getString("email");
                 username = rs.getString("username");
                 type = rs.getString("type");
+            } else {
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "There is no sush employee", null);
+                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
             }
-            else {
-                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR,"There is no sush employee", null);
-                FacesContext.getCurrentInstance().addMessage(null,  facesMessage);
-            }
-    
-        }
-        catch (Exception ex){
-            String message = ex.getMessage(); 
+
+        } catch (Exception ex) {
+            String message = ex.getMessage();
             FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null);
-            FacesContext.getCurrentInstance().addMessage(null,  facesMessage);
-        }
-        finally{
-            try{
-                
-            }catch(Exception ex){
-                
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+        } finally {
+            try {
+
+            } catch (Exception ex) {
+
             }
         }
     }
+
     /**
      * update function takes all the info of the user and inserts it in the DB
      */
     public void update() {
         DB db = new DB();
         try {
-            
+
             String sql = "update users set fullname=?,address=?, email=?, phone=? where username=? ";
             db.update(sql, name, address, email, phone, username);
-            
+
             FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Record is Updated", null);
-            FacesContext.getCurrentInstance().addMessage(null,  facesMessage);
-        }
-        catch (Exception ex){
-            String message = ex.getMessage(); 
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+        } catch (Exception ex) {
+            String message = ex.getMessage();
             FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null);
-            FacesContext.getCurrentInstance().addMessage(null,  facesMessage);
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
         }
     }
-    
+
     /**
      * delete function deletes the user based on his UserName
      */
-    public void delete(){
-        
+    public void delete() {
+
         DB db = new DB();
         try {
-            
+
             String sql = "delete from users where username=? ";
-            db.update(sql,username);
-            
+            db.update(sql, username);
+
             FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Record is deleted", null);
-            FacesContext.getCurrentInstance().addMessage(null,  facesMessage);
-        }
-        catch (Exception ex){
-            String message = ex.getMessage(); 
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+        } catch (Exception ex) {
+            String message = ex.getMessage();
             FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null);
-            FacesContext.getCurrentInstance().addMessage(null,  facesMessage);
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
         }
     }
-    
-    
+
     public void reset() {
         name = "";
         address = "";
